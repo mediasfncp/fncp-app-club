e734220b8d358253b4c0ff8a889bb2ea510d25f9
mediasfncp <medias.fncp@hotmail.com>
2026-03-18 22:49:44 +0100

    Implement Stripe checkout session handler

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,1 +1,18 @@
+import Stripe from "stripe"
 
+const stripe = new Stripe(process.env.STRIPE_SECRET)
+
+export default async function handler(req,res){
+
+ const session = await stripe.checkout.sessions.create({
+  mode:"payment",
+  line_items:[{
+    price:process.env.PRICE_1,
+    quantity:1
+  }],
+  success_url:process.env.URL,
+  cancel_url:process.env.URL
+ })
+
+ res.json({url:session.url})
+}
