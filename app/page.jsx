5d185cb9eacc8cca064b4145476c5c27c997c259
mediasfncp <medias.fncp@hotmail.com>
2026-03-18 22:15:59 +0100

    Add Home component for FNCP Club de Plage

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function Home(){
+
+ return(
+  <div className="center">
+
+   <h1>🏖️ FNCP – Club de Plage</h1>
+
+   <a href="/booking">
+    <button className="btn">Réserver une séance</button>
+   </a>
+
+  </div>
+ )
+}
